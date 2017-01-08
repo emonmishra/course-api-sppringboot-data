b6a556f47e21790dce39912d4ab877842ac73f8a
Emon Mishra <mishraemon@gmail.com>
2017-01-08 23:05:19 +0530

    set up

---
+++ src/main/java/github/emon/springboot/topics/TopicService.java
@@ -0,0 +1,48 @@
+package github.emon.springboot.topics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
+			new Topic("angular", "Angular 2", "Angular 2 Course Quickstart"),
+			new Topic("django", " django frawemwork", "Django Frame Work in Detail")));
+
+	public List<Topic> getAllTopics() {
+
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public boolean addTopic(Topic topic) {
+
+		return topics.add(topic);
+	}
+
+	public Topic updateTopic(Topic topic, String id) {
+
+		for (int i = 0; i < topics.size(); i++) {
+
+			if (topics.get(i).getId().equals(id)) {
+				return topics.set(i, topic);
+			}
+		}
+
+		return null;
+	}
+
+	public boolean deleteTopic(String id) {
+
+		return topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
---
+++ src/main/java/github/emon/springboot/topics/TopicsController.java
@@ -0,0 +1,45 @@
+package github.emon.springboot.topics;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicsController {
+
+	@Autowired
+	private TopicService topicService;
+
+	@RequestMapping("/topics")
+	public List<Topic> getTopics() {
+
+		return topicService.getAllTopics();
+	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable("id") String id) {
+
+		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public boolean addTopic(@RequestBody Topic topic) {
+		return topicService.addTopic(topic);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public Topic updateTopic(@RequestBody Topic topic, @PathVariable("id") String id) {
+		return topicService.updateTopic(topic, id);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public boolean deleteTopic(@PathVariable("id") String id) {
+		return topicService.deleteTopic(id);
+	}
+
+}
